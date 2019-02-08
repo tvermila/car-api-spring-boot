be96533dbfefc493d8d2a237c7b429d03b19484c
Tomas Vermilä <tomas.vermila@myy.haaga-helia.fi>
2019-02-08 11:54:12 +0200

    Added project, swagger and edited .gitignore

---
+++ src/main/java/fi/tvermila/carapispringboot/CarApiSpringBootApplication.java
@@ -0,0 +1,14 @@
+package fi.tvermila.carapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarApiSpringBootApplication.class, args);
+	}
+
+}
+
