fff34f346dd4e68fda9d0b0beda9f4069d5229f4
Tomas Vermilä <tomas.vermila@myy.haaga-helia.fi>
2019-02-08 13:33:13 +0200

    Added filter for make and fixed seed data

--- src/main/java/fi/tvermila/carapispringboot/car/CarController.java
+++ src/main/java/fi/tvermila/carapispringboot/car/CarController.java
@@ -1,9 +1,12 @@
 package fi.tvermila.carapispringboot.car;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +16,14 @@ public class CarController {
 	private CarRepository carRepository;
 	
 	@GetMapping("/cars")
-	public List<Car> getCars() {
+	public List<Car> getCars(@RequestParam(required=false) String make) {
 		List<Car> cars = carRepository.findAll();
+		
+		if (make != null) {
+			cars = cars.stream()
+					.filter(car -> car.getMake().equals(make))
+					.collect(Collectors.toList());
+		}
 		
 		return cars;
 	}
